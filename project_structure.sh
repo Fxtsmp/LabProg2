f483dae7f294a3a5d39e80a9333edeabc1d3b24d
eduarsc <esebastiancprog@gmail.com>
2021-11-12 08:40:12 -0300

    created project basic structure and update html

---
+++ project_structure.sh
@@ -0,0 +1,8 @@
+mkdir css && mkdir js && mkdir images
+cd css && echo > style.css && cd ..
+cd js && echo > script.js && cd ..
+echo > index.html
+tree
+echo structure project created - eduars v1.0
+echo write your code dude XD
+code .
